d12eb4592417a24e012ea2e912a5f46a7880286a
love2310216 <303628252@qq.com>
2025-01-05 11:18:01 +0800

    Create my.php

---
+++ my.php
@@ -0,0 +1,23 @@
+<?php
+$url = $_GET['url'] ?? '';
+if (strpos($url, 'http') !== 0) {
+        $url = base64_decode($url);
+    }
+$html_code = pachongbo($url);
+echo $html_code;
+function pachongbo($url){
+    $headers = [
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data; 
+}       
+?>
